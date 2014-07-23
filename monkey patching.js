d9565f8ab5b5e2b78ffde2dd01478c23d46ae735
Qing Sheng <shengoo@hotmail.com>
2014-07-23 16:56:30 +0800

    added      monkey patching.js

---
+++ monkey patching.js
@@ -0,0 +1,18 @@
+var obj = {
+	sayHi:function  () {
+		console.log("hi");
+	}
+}
+
+function trace(o,m){
+	var original = o[m];	// save original function
+	o[m] = function  () {
+		console.log(new Date(), "Entering:",m);
+		var result = original.apply(this,arguments);	// invoke original function
+		console.log(new Date(),"Exiting:",m);
+		return result;
+	};
+}
+
+trace(obj,"sayHi");
+obj.sayHi()
