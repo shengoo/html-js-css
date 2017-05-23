5bbd91caf683500b911511e458767ce8a9f9697f
shengoo <shengoo@hotmail.com>
2017-05-23 22:59:09 +0800

    add files

--- inhert/inheritfun.js
+++ inhert/inheritfun.js
@@ -11,7 +11,9 @@ function B(){
 }
 
 // B.prototype = A.prototype;
-B.prototype = new A();
+// B.prototype = new A();
+B.prototype = Object.create(A.prototype)
+B.prototype.constructor = A;
 var b = new B();
 b.fa();
 b.fb();
