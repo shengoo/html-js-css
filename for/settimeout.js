43b297cbe983988b2e4f6a5fe064cbe156953b19
Sheng Qing <qing.sheng@thomsonreuters.com>
2017-08-25 17:00:31 +0800

    add

--- for/settimeout.js
+++ for/settimeout.js
@@ -1,5 +1,10 @@
 for (var i = 0; i < 10; ++i) {
-    setTimeout(function () {console.log(i)}, 0);
+	var j = i;
+    setTimeout(function () {console.log(j)}, 0);
+}
+for (var i = 0; i < 10; ++i) {
+	const j = i;
+    setTimeout(function () {console.log(j)}, 0);
 }
 
 for (var i = 0; i < 10; ++i) {
